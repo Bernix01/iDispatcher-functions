3f4150e1d5ad4f21a7f6c48fd23af1066712c72f
Guillermo Enrique Bernal Moreira <gbernal096@gmail.com>
2019-03-22 14:12:28 -0500

    updated functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,12 +5,6 @@ admin.initializeApp(functions.config().firebase);
 
 const db = admin.firestore();
 db.settings({ timestampsInSnapshots: true });
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
 
 export const deactivateUser = functions.auth.user().onDelete(async user => {
   const usersRef = db.collection("users");
@@ -24,9 +18,8 @@ export const deactivateUser = functions.auth.user().onDelete(async user => {
 });
 
 export const report = functions.https.onCall(
-  async (data: { duid: string; action: string }, context) => {
-    const { duid, action } = data;
-    const uid = context.auth.uid;
+  async (data: { uid: string; duid: string; action: string, operationId:string }, context) => {
+    const { duid, action, uid, operationId } = data;
     const userDoc = await db
       .collection("users")
       .doc(uid)
@@ -48,8 +41,10 @@ export const report = functions.https.onCall(
       device,
       formula,
       action: action,
+      operationId,
       when: new Date()
     };
+    console.info(reportData)
     await db.collection("reports").add(reportData);
     return true;
   }
@@ -62,11 +57,37 @@ export const fetchDevice = functions.https.onCall(async (data, context) => {
     .doc(deviceUID)
     .get();
   const dd = d.data();
-  console.info(dd);
   const f = await db
     .collection("formulas")
     .doc(dd.formula)
     .get();
   const ff = f.data();
-  return { device: dd, formula: ff };
+  return { device: { ...dd, id: deviceUID }, formula: ff };
+});
+
+export const doAuth = functions.https.onCall(async (data, context) => {
+  const deviceUID = data.duid;
+  const userUID: string = data.uid;
+  if (!deviceUID || !userUID) {
+    return { succces: false, user: null };
+  }
+  const u = await db
+    .collection("users")
+    .doc(userUID.replace("\n", ""))
+    .get();
+  const d = await db
+    .collection("devices")
+    .doc(deviceUID)
+    .get();
+  const dd = d.data();
+  const user = u.data();
+  if (!user || !dd) {
+    return { success: false, user: null };
+  }
+
+  if (!user.active || user.org !== dd.org) {
+    return { success: false, user: null };
+  }
+
+  return { success: user !== undefined, user: { ...user, id: userUID.replace("\n", "") } };
 });
